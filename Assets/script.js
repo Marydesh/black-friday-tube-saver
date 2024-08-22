9276225ca5b432a9b32910ed2fd276acb85960fe
Marydesh <144630405+Marydesh@users.noreply.github.com>
2024-08-21 20:49:47 -0400

    Update script.js - removed the SDK script

--- Assets/script.js
+++ Assets/script.js
@@ -112,26 +112,6 @@ async function searchOpenLibrary(searchInputVal) {
     cardDiv.append(bookCover)
     cardDiv.append(appendLink)
 
-    import { datadogRum } from '@datadog/browser-rum';
-
-datadogRum.init({
-    applicationId: 'bf22aa48-0174-4f82-973e-19014254f823',
-    clientToken: 'pub7e974f1ddc9d10ecada77fef5a11110d',
-    // `site` refers to the Datadog site parameter of your organization
-    // see https://docs.datadoghq.com/getting_started/site/
-    site: 'datadoghq.com',
-    service: 'book-buddy-base',
-    env: 'prod',
-    // Specify a version number to identify the deployed version of your application in Datadog
-    // version: '1.0.0',
-    sessionSampleRate: 100,
-    sessionReplaySampleRate: 20,
-    trackUserInteractions: true,
-    trackResources: true,
-    trackLongTasks: true,
-    defaultPrivacyLevel: 'mask-user-input',
-});
-
     function appendProperty(title, value) {
       let p = document.createElement('p')
       p.innerHTML = `${title}: ${value}`
